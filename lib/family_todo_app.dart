1d2d405adb0721d9245c653aa6e672aea0c0e7be
Ovidio Calvet <calveto@gmail.com>
2019-01-08 00:08:42 -0500

    Connect bloc

--- lib/family_todo_app.dart
+++ lib/family_todo_app.dart
@@ -1,3 +1,5 @@
+import 'package:family_todo/blocs/bloc_provider.dart';
+import 'package:family_todo/blocs/todo_bloc.dart';
 import 'package:family_todo/screens/create_todo_screen.dart';
 import 'package:family_todo/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,15 +7,19 @@ import 'package:flutter/material.dart';
 class FamilyTodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      title: 'Family Todo',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomeScreen(),
-        '/create-todo': (context) => CreateTodoScreen(),
-      },
+    TodoBloc bloc = TodoBloc();
+    return BlocProvider<TodoBloc>(
+      bloc: bloc,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        title: 'Family Todo',
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeScreen(),
+          '/create-todo': (context) => CreateTodoScreen(),
+        },
+      ),
     );
   }
 }
