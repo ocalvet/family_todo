8aee31baf4f7920a4b97758463ee260e004c9d8f
Ovidio Calvet <calveto@gmail.com>
2019-01-08 00:19:21 -0500

    Connect create todo to bloc

--- lib/screens/create_todo_screen.dart
+++ lib/screens/create_todo_screen.dart
@@ -1,11 +1,40 @@
+import 'package:family_todo/blocs/bloc_provider.dart';
+import 'package:family_todo/blocs/todo_bloc.dart';
 import 'package:flutter/material.dart';
 
 class CreateTodoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TodoBloc bloc = BlocProvider.of<TodoBloc>(context);
     return Scaffold(
-      appBar: AppBar(title: Text('New Todo'),),
-      body: Text('Create New Todo'),
-    );
+        appBar: AppBar(
+          title: Text('New Todo'),
+        ),
+        body: Column(
+          children: <Widget>[
+            StreamBuilder(
+              stream: bloc.todoTitle$,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                return TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Todo title',
+                  ),
+                  onChanged: bloc.changeTitle,
+                );
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: 'Todo description'),
+              onChanged: bloc.changeDescription,
+            ),
+            RaisedButton(
+              child: Text('Submit'),
+              onPressed: () {
+                print('Saving todo');
+                Navigator.pushNamed(context, '/');
+              },
+            )
+          ],
+        ));
   }
 }
