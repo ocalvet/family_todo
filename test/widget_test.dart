5ebab8995ec554241cceb9324b1f8715f70633e4
Ovidio Calvet <calveto@gmail.com>
2019-01-13 16:31:13 -0500

    Fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:family_todo/family_todo_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Finds a widget with the word "Home" in it', (WidgetTester tester) async {
     await tester.pumpWidget(FamilyTodoApp());
     expect(find.text('Home'), findsOneWidget);
   });
